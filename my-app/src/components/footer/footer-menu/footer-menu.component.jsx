886491af279b522e99e4b01438bbc1c412ac0166
drocpdp <davidreynon@gmail.com>
2025-01-21 17:43:51 +0100

    fixed hard links

--- my-app/src/components/footer/footer-menu/footer-menu.component.jsx
+++ my-app/src/components/footer/footer-menu/footer-menu.component.jsx
@@ -5,9 +5,9 @@ const FooterMenuComponent = () => {
     return (
         <div class='bottom-menu'>
             <a href=''>About Me</a>  |  
-            <a href=''> LinkedIn</a>  |  
-            <a href=''> Github</a>  |  
-            <a href=''> Bitbucket</a> -- Copyright 2025 - David Eynon
+            <a href='https://linkedin.com/in/daveqa' target="_blank"> LinkedIn</a>  |  
+            <a href='https://github.com/drocpdp' target="_blank"> Github</a>  |  
+            <a href='https://bitbucket.com/drocpdp' target="_blank"> Bitbucket</a> -- Copyright 2025 - David Eynon
     </div>        
     );
 };
--- my-app/src/components/header-container/header-menu/header-menu.component.jsx
+++ my-app/src/components/header-container/header-menu/header-menu.component.jsx
@@ -4,10 +4,10 @@ import './header-menu.styles.scss'
 const HeaderMenuComponent = () => {
     return (
         <div class='top-menu'>
-            <a class='top-title-href' href="" target="_blank">About Me</a>  |  
-            <a class='top-title-href' href="" target="_blank"> LinkedIn</a>  |  
-            <a class='top-title-href' href="" target="_blank"> Github</a>  |  
-            <a class='top-title-href' href="" target="_blank"> Bitbucket</a>
+            <a class='top-title-href' href="" >About Me</a>  |  
+            <a class='top-title-href' href="https://linkedin.com/in/daveqa" target="_blank"> LinkedIn</a>  |  
+            <a class='top-title-href' href="https://github.com/drocpdp" target="_blank"> Github</a>  |  
+            <a class='top-title-href' href="https://bitbucket.com/drocpdp" target="_blank"> Bitbucket</a>
         </div>
     )
 }
