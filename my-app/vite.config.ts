bad56e8604f05ea6a44121159ea8b724c790f746
drocpdp <davidreynon@gmail.com>
2025-05-10 17:47:36 +0200

    vite fixes

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   base: '/',
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'dist/index.html',
-          dest: '.', 
-          rename: '404.html'
-        }
-      ]
-    })
-  ]
+  plugins: [react()],
 });
